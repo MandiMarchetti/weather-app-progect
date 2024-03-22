ecadb42c7d0435a24d561145ff76bb18a34b098e
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-03-22 16:52:54 +0100

    First commit

---
+++ src/Components/Main.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+
+const Main = () => {
+  const [city, setCity] = useState("");
+  const [weatherForecast, setWeatherForecast] = useState(null);
+
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSearch = () => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=664109e99631f71353c34842fd89ab24&lang=en`)
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        setWeatherForecast(data);
+      });
+  };
+
+  return (
+    <div>
+      <main className="container">
+        <div className="jumbotron">
+          <h1>Is it a sunny day?😎 </h1>
+          <p className="lead">Check right now the weather in your city...</p>
+
+          <div className="row mb-4 justify-content-center">
+            <div className="col-md-6 ">
+              <input
+                onChange={handleChange}
+                className="form-control"
+                placeholder="Type her the name of your city..."
+                value={city}
+              />
+            </div>
+          </div>
+
+          <button onClick={handleSearch} className="btn btn-warning btn-lg">
+            Search 🕶️
+          </button>
+        </div>
+
+        {weatherForecast ? (
+          <div>
+            <div className="mt-4">
+              <div>
+                <h3>Today is : {weatherForecast.weather[0].description}</h3>
+                <p>You probably are feeling like: {weatherForecast.main.feels_like}°F</p>
+                <p>The humidity is: {weatherForecast.main.humidity}%</p>
+                <p>So, the Max temperature will be: {weatherForecast.main.temp_max}°F...</p>
+                <p>...and the Min temperature will be: {weatherForecast.main.temp_min}°F...</p>
+              </div>
+
+              <div>
+                <img src={()=>{
+                    if({weatherForecast.main.temp_max} >=)
+                }} />
+              </div>
+            </div>
+          </div>
+        ) : null}
+      </main>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <a className="navbar-brand text-white" href="#top">
+          Amy Weather ☀️
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
